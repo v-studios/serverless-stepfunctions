388b756fd564df210253abe9e456cdf3ccbdd7a0
Chris Shenton <chris@shenton.org>
2019-07-27 17:41:22 -0400

    Need plugsin to read steps

--- chris-callback/handler.py
+++ chris-callback/handler.py
@@ -14,15 +14,17 @@ def start_task_and_wait_for_callback(event, context):
 
         print(f'Calling Step Functions to complete callback with {params}')
         # output is the JSON output of the task as a str
-        res = sfn.send_task_success(taskToken=task_token, output=
+        res = sfn.send_task_success(taskToken=task_token,
+                                    output=json.dumps({'msg': 'WHATEVER'}))
         print(f'sendTaskSuccess res={res}')
         # send_task_failure(taskToken=..., error='...', cause='...')
 
-def notify_success(event, context):
-    print(f'success: event={event}')
-    return {'msg': 'Looking Good'}
+
+# def notify_success(event, context):
+#     print(f'success: event={event}')
+#     return {'msg': 'Looking Good'}
 
 
-def notify_failure(event, context):
-    print(f'failure: event={event}')
-    return {'msg': 'Failure Dude'}
+# def notify_failure(event, context):
+#     print(f'failure: event={event}')
+#     return {'msg': 'Failure Dude'}
