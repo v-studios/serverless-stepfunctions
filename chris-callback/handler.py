ab1a037bf0ba89e3c925a64052cad7229fe72652
Chris Shenton <chris@shenton.org>
2019-08-07 10:18:36 +0200

    Trigger with HTTP instead of manual, return token

--- chris-callback/handler.py
+++ chris-callback/handler.py
@@ -11,7 +11,8 @@ def start_task_and_wait_for_callback(event, context):
     # Here we would do something useful, then continue the statemachine
     print(f'sending success...')
     res = sfn.send_task_success(taskToken=task_token,
-                                output=json.dumps({'msg': 'WHATEVER DUDE'}))
+                                output=json.dumps({'msg': 'WHATEVER DUDE',
+                                                   'taskToken': task_token}))
     print(f'sendTaskSuccess res={res}')
     # send_task_failure(taskToken=..., error='...', cause='...')
     return {'msg': 'does this really get sent anywhere?'}
